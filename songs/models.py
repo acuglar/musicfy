238a099653b80f97fff63524651803c9ce97ea15
Alexandre Uglar <acuglar@gmail.com>
2021-08-27 02:37:47 -0300

    models and relations

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    """ artist 1:N songs """
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey("Artist", on_delete=models.CASCADE, related_name="songs")   
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Biography(models.Model):
+    """ artist 1:1 bio """
+    description = models.TextField()
+    artist = models.OneToOneField(Artist, on_delete=models.CASCADE)
+
+
+class Playlist(models.Model):
+    """ song N:N playlist """
+    title = models.CharField(max_length=255)
+    songs = models.ManyToManyField(Song, related_name="playlists")
